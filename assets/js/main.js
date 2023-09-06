51128aa1e91ae0642e69f96e1a165c95950a4d07
Lucas Alves <Lucasasdev@gmail.com>
2023-09-06 15:34:48 -0300

    Separando o consumo da API da manipulação de HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 
-const offset = 0
-const limit = 10
+const offset = 0;
+const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
 function convertPokemonToLi(pokemon) {
@@ -25,13 +25,15 @@ function convertPokemonToLi(pokemon) {
 const pokemonList = document.getElementById('pokemonList')
 
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => { 
+    pokeApi.getPokemons().then((pokemons) => { 
+
+        const listItems = []
+
+        pokemons.map()
+
         for (let i = 0; i < pokemons.length; i++) {
             const pokemon = pokemons[i];
-            pokemonList.innerHTML += convertPokemonToLi(pokemon)
+            listItems.push(convertPokemonToLi(pokemon))   
         }
+        console.log(listItems)
     })
-    .catch((error) => console.error(error))
