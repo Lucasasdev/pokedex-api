58fa62876011b2937ba6c368b647fc47866fa799
Lucas Alves <Lucasasdev@gmail.com>
2023-09-16 15:39:42 -0300

    Primeira requisição usando fetch()

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,11 @@
 
-console.log("sucesso!")
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+//Método fetch indo pegar o endpoint, quando ele pegar o endpoint vai trazer a resposta, enquando isso o código segue em frente.
+fetch(url).then(function (response) {
+    console.log(response)
+})
+
+var soma = 10 + 10
+console.log(soma)
