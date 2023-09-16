a746e9b8905b23059c46335a4becf81e1a4dcaf6
Lucas Alves <Lucasasdev@gmail.com>
2023-09-16 16:26:20 -0300

    Manipulando o resultado da requisição através de uma Promise, encadinhando os then e simplificando as funções

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,8 @@
 const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-//Método fetch indo pegar o endpoint, quando ele pegar o endpoint vai trazer a resposta, enquando isso o código segue em frente.
-fetch(url).then(function (response) {
-    console.log(response)
-})
 
-var soma = 10 + 10
-console.log(soma)
+fetch(url).then((response) => response.json())
+.then((response) => console.log(response))
+.catch((error) => console.error(error))
+
