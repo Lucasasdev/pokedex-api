9d562e82a8467b3d99e222ec9849e73f8a6ce94c
Lucas Alves <Lucasasdev@gmail.com>
2023-09-16 21:05:45 -0300

    Tranformando a PokemonList que é uma array em um lista HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,33 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
+function convertPokemonToHtml(pokemon) {
+    return `<li class="pokemon">
+    <span class="number">#001</span>
+    <span class="name">${pokemon.name}</span>
+
+    <div class="detail">
+        <ol class="types">
+            <li class="type">grass</li>
+            <li class="type">poison</li>
+        </ol>
+
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+
+    </div>
+</li>`
+}
+
 fetch(url).then((response) => response.json())
-.then((response) => console.log(response))
+.then((jsonBody) => jsonBody.results)
+.then((pokemonList) => {
+    
+    for (let index = 0; index < pokemonList.length; index++) {
+        const pokemon = pokemonList[index];
+        console.log(convertPokemonToHtml(pokemon))
+        
+    }
+
+})
 .catch((error) => console.error(error))
 
