f4d96d51a46c963fd8ea4dd20b85ec57c77447cb
Lucas Alves <Lucasasdev@gmail.com>
2023-09-21 15:31:15 -0300

    Convertendo o modelo de PokeApi para nosso modelo

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,9 +1,25 @@
-//consumo e tratamento da API
+
 const pokeApi = {}
+
+function getConvertPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.order
+    pokemon.name = pokeDetail.name
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+} 
 
 pokeApi.getPokemonDetails = (pokemon) => {
     return fetch(pokemon.url)
-    .then((response) => response.json())
+            .then((response) => response.json())
+            .then(getConvertPokeApiDetailToPokemon)
 }
 
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
